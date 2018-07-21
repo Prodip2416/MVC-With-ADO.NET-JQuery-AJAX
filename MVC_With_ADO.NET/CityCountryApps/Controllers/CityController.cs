9dcaf80fb30cf56aea20e81b983c860bf5ec8b64
Prodip Sarker <prodip_sarker@diu.edu.bd>
2018-07-21 19:53:39 +0600

    Add files via upload

---
+++ MVC_With_ADO.NET/CityCountryApps/Controllers/CityController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CityCountryApp.Models;
+using CityCountryApp.Repository;
+
+namespace CityCountryApp.Controllers
+{
+    public class CityController : Controller
+    {
+       CityRepository cityRepository = new CityRepository();
+       CountryRepository countryRepository= new CountryRepository();
+        public ActionResult Index()
+        {
+            return View(cityRepository.GetAllCityDetails());
+        }
+
+        // GET: City/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(cityRepository.GetAllCityDetails().Find(x=>x.Id==id));
+        }
+
+        // GET: City/Create
+        public ActionResult Create()
+        {
+            ViewBag.CountryId = new SelectList(countryRepository.GetAllCountry(),"id","name");
+            return View();
+        }
+
+        // POST: City/Create
+        [HttpPost]
+        public ActionResult Create(City city)
+        {
+            try
+            {
+                ViewBag.CountryId = new SelectList(countryRepository.GetAllCountry(), "id", "name");
+                if (cityRepository.AddCity(city))
+                {
+                    return RedirectToAction("Index");
+                }
+                return View();
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: City/Edit/5
+        public ActionResult Edit(int id)
+        {
+            int Countryid = cityRepository.GetCountryId(id);
+            ViewBag.CountryId = new SelectList(countryRepository.GetAllCountry(), "id", "name",Countryid);
+            return View(cityRepository.GetAllCityDetails().Find(x => x.Id == id));
+        }
+
+        // POST: City/Edit/5
+        [HttpPost]
+        public ActionResult Edit(City city)
+        {
+            try
+            {
+                ViewBag.CountryId = new SelectList(countryRepository.GetAllCountry(), "id", "name");
+                cityRepository.UpdateCity(city);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: City/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(cityRepository.GetAllCityDetails().Find(x => x.Id == id));
+        }
+
+        // POST: City/Delete/5
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult Delete_Post(int id)
+        {
+            try
+            {
+                cityRepository.DeleteCity(id);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVC_With_ADO.NET/CityCountryApps/Controllers/CountryController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CityCountryApp.Models;
+using CityCountryApp.Repository;
+
+namespace CityCountryApp.Controllers
+{
+    public class CountryController : Controller
+    {
+       CountryRepository countryRepository= new CountryRepository();
+        public ActionResult Index()
+        {
+            return View(countryRepository.GetAllCountry());
+        }
+
+        // GET: Country/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(countryRepository.GetAllCountry().Find(x=>x.Id==id));
+        }
+
+        // GET: Country/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Country/Create
+        [HttpPost]
+        public ActionResult Create(Country country)
+        {
+            try
+            {
+                if (countryRepository.AddCountry(country))
+                {
+                    return RedirectToAction("Index");
+                }
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Country/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(countryRepository.GetAllCountry().Find(x => x.Id == id));
+        }
+
+        // POST: Country/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Country country)
+        {
+            try
+            {
+                countryRepository.UpdateCountry(country);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Country/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(countryRepository.GetAllCountry().Find(x => x.Id == id));
+        }
+
+        // POST: Country/Delete/5
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult Delete_Post(int id)
+        {
+            try
+            {
+                countryRepository.DeleteCountry(id);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
